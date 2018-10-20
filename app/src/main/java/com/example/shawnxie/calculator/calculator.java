c738d8fb9aa40e9a54602a4c0419b3c354cc3676
identity914 <804810435@qq.com>
2018-10-20 16:56:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shawnxie/calculator/calculator.java
@@ -0,0 +1,13 @@
+package com.example.shawnxie.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
